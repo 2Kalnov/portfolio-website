93009b27abd401908c64ecc5cfd370c8bdfb78f1
Nikita <jdmcorolla21@gmail.com>
2018-08-20 13:10:51 +0300

    Implemented setting the image defaults for portfolio items via model callback

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
 
   scope :backend, lambda { where subtitle: 'Backend project' }
   scope :frontend, lambda { where subtitle: 'Frontend project' }
+
+  after_initialize :set_defaults
+
+  def set_defaults 
+    self.main_image ||= "http://placehold.it/500x300"
+    self.thumb_image ||= "http://placehold.it/250x150"
+  end
 end
